a392942d0e77a80e60ba242269ec673e2baf4300
Pranoy Tez <pranoytez@gmail.com>
2021-01-23 13:15:49 -0600

    First Commit | Adding starter files

---
+++ src/main/java/com/tez/collabList/CollabListApplication.java
@@ -0,0 +1,13 @@
+package com.tez.collabList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tez/collabList/CollabListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tez.collabList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
